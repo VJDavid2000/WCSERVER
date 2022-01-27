565114e1d7e76ffd306a78c414179189f54f3d7d
VJDavid2000 <98504050+VJDavid2000@users.noreply.github.com>
2022-01-27 16:13:23 +0800

    Add files via upload

---
+++ 1-Demos/demo-1.js
@@ -0,0 +1,9 @@
+/*David, Vincent Jericho C.
+2075 - WCSERVER
+Create A Function*/
+
+function Display(x) {
+  console.log(x)
+}
+
+Display(100);
---
+++ 1-Demos/demo-2.js
@@ -0,0 +1,12 @@
+/*David, Vincent Jericho C.
+2075 - WCSERVER*/
+
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+  
+  //Any Path or Code Here
+
+});
+
+server.listen(3000);
